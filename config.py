c25fd82ce705d5bdce600e1fbe07f58dccc01c13
ThatDudeJude <judegachoki@gmail.com>
2022-02-01 23:51:38 +0300

    unhid config file

---
+++ config.py
@@ -0,0 +1,20 @@
+from instance.config import BOOKS_CSV
+
+
+class Config(object):
+    TESTING=False 
+    DEBUG=False 
+    INITIALIZE_DB_FILE='./bibliophiliac/schema.sql'
+
+class ProductionConfig(Config):
+    pass 
+
+class DevelopmentConfig(Config):
+    DEBUG=True
+
+class TestingConfig(Config):
+    TESTING=True 
+    DEBUG=True 
+    DATABASE_URL="postgresql:///test_bibliophiliac"
+    TEST_DB_FILE='./tests/dbase_test.sql'
+    BOOKS_CSV='../tests/test_books.csv'
---
+++ instance/config.py
@@ -0,0 +1,10 @@
+SECRET_KEY = "f7f8f089b8f85bc8c82bc46a31d9cf5798e8219e65d5cd48ca171c36ae600180"
+API_KEY = None 
+DATABASE_URL="postgresql:///bibliophiliac_db"
+SESSION_PERMANENT=False 
+BOOKS_CSV = "books.csv"
+AVATARS_FOLDER = '/bibliophiliac/static/imgs/avatars'
+DEFAULT_AVATAR_IMAGE= '/bibliophiliac/static/imgs/default_avatar.png'
+# MAX_CONTENT_LENGTH = 7 * 1000 * 1000
+# SESSSION_TYPE='filesystem'
+
