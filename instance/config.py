3cd16b06d252be58e189f5c50ccfe0b46440e10d
ThatDudeJude <judegachoki@gmail.com>
2022-02-08 01:44:56 +0300

    switched database

--- instance/config.py
+++ instance/config.py
@@ -1,7 +1,7 @@
 SECRET_KEY = "f7f8f089b8f85bc8c82bc46a31d9cf5798e8219e65d5cd48ca171c36ae600180"
 API_KEY = None 
-DATABASE_URL="postgresql:///bibliophiliac_db"
-# DATABASE_URL="postgresql://aibzdsyfyztare:053c8a31cc2b6860e8e0de0eeed90fed4906595fb6cd703b8a57bc74ef0bc510@ec2-23-21-229-200.compute-1.amazonaws.com:5432/d8im2d1d2orr32"
+# DATABASE_URL="postgresql:///bibliophiliac_db"
+DATABASE_URL="postgresql://aibzdsyfyztare:053c8a31cc2b6860e8e0de0eeed90fed4906595fb6cd703b8a57bc74ef0bc510@ec2-23-21-229-200.compute-1.amazonaws.com:5432/d8im2d1d2orr32"
 SESSION_PERMANENT=False 
 BOOKS_CSV = "books.csv"
 AVATARS_FOLDER = '/bibliophiliac/static/imgs/avatars'
