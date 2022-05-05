af0968b29e0dd69a3e80253d13243cf46cbe7581
ThatDudeJude <judegachoki@gmail.com>
2022-05-06 00:24:51 +0300

    Increased Request Timeout

--- instance/config.py
+++ instance/config.py
@@ -1,6 +1,6 @@
 SECRET_KEY = "f7f8f089b8f85bc8c82bc46a31d9cf5798e8219e65d5cd48ca171c36ae600180"
 BOOKS_API_KEY = "AIzaSyDniynUGFYHQi2ooiC-Q9G9PUDvu-TKVbY"
-DATABASE_URL="postgresql:///bibliophiliac_db"
+DATABASE_URL="postgres:///bibliophiliac_db"
 # DATABASE_URL="postgresql://aibzdsyfyztare:053c8a31cc2b6860e8e0de0eeed90fed4906595fb6cd703b8a57bc74ef0bc510@ec2-23-21-229-200.compute-1.amazonaws.com:5432/d8im2d1d2orr32"
 SESSION_PERMANENT=False 
 # MAX_CONTENT_LENGTH = 7 * 1000 * 1000
